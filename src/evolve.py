925fb5ddb0e95e1b09e94a74a5a633d8cc46d58d
Amoli Kotalwar <amolikotalwar@gmail.com>
2016-03-29 10:52:28 +0530

    evolution

--- src/evolve.py
+++ src/evolve.py
@@ -2,12 +2,16 @@
 Population = []
 
 
-def Population_seed(self, Population, Schedule):
+def Population_seed(self, Population, meta):
     """
     Randomly seeds schedules to create a population.
     
     """
-
+    total_population = len(Population)
+    for i in range (total_population + 1):
+        s = Schedule(meta['time'], meta['room'],meta['allocations'])
+        s.seed_random()
+        Population.append(s)
     
 
 
@@ -23,19 +27,19 @@ def Population_evolve(self, Population, Schedule, generations, fitness):
     """
 
     Population =  Population.sort(reverse=True, key = Schedule.fitness())
-    max_fitness = float("-inf")
+    max_fitness = Population[0].fitness
     elapsed_generations = 0
     while(max_fitness < fitness and elapsed_generations < generations):
         del Population[-1]
         del Population[-2]
-        crossover(Population[0], Population[1])
+        child1, child2 = crossover(Population[0], Population[1])
         Population.append(child1)     
         Population.append(child2)
         mutate(Population[-1])
         mutate(Population[-2])
-        Population =  Population.sort(reverse=True, key = Schedule.fitness())
+        Population =  Population.sort(reverse=True, key = Schedule.fitness)
         fittest_schedule = Population[0]
-        max_fitness = fittest_schedule.fitness()
+        max_fitness = fittest_schedule.fitness
         elapsed_generations = elapsed_generations + 1
 
 
