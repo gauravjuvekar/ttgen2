4fc5550f5b3308b14c8a76e252b7a8a350596893
Amoli Kotalwar <amolikotalwar@gmail.com>
2016-03-29 09:32:02 +0530

    evolution

---
+++ src/evolve.py
@@ -0,0 +1,54 @@
+
+Population = []
+
+
+def Population_seed(self, Population, Schedule):
+    """
+    Randomly seeds schedules to create a population.
+    
+    """
+
+    
+
+
+
+
+
+def Population_evolve(self, Population, Schedule, generations, fitness):
+    """
+    Evolves the population by repeated crossover of the fittest,
+    mutation and death of the least fit schedules till a certain number of
+    generations have completed or till a schedule with required fitness is obtained.
+
+    """
+
+    Population =  Population.sort(reverse=True, key = Schedule.fitness())
+    max_fitness = float("-inf")
+    elapsed_generations = 0
+    while(max_fitness < fitness && elapsed_generations < generations):
+        del Population[-1]
+        del Population[-2]
+        crossover(Population[0], Population[1])
+        Population.append(child1)     
+        Population.append(child2)
+        mutate(Population[-1])
+        mutate(Population[-2])
+        Population =  Population.sort(reverse=True, key = Schedule.fitness())
+        fittest_schedule = Population[0]
+        max_fitness = fittest_schedule.fitness()
+        elapsed_generations = elapsed_generations + 1
+
+
+def Population_free(Population):
+    del Population    
+
+
+
+
+
+
+
+
+
+
+
