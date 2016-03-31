fbe00d05f42d17667e1331e6165627af7485133b
Abhishek Lolage <abhisheklolage@gmail.com>
2016-03-31 11:52:24 +0530

    Update schedule.py

--- src/schedule.py
+++ src/schedule.py
@@ -160,7 +160,7 @@ def crossover(self, schedule_1, schedule_2, count):
     """
     cross_point_1 = random.randrange(self._n_slots)
     cross_point_2 = random.randrange(self._n_slots)
-    while ((cross_point_2 == cross_point_1) or ((cross_point_2 - cross_point_1) == abs(_n_slots - 1))):
+    while ((cross_point_2 == cross_point_1) or (abs(cross_point_2 - cross_point_1) == abs(_n_slots - 1))):
         cross_point_2 = random.randrange(self._n_slots)
     
     swap_chunk(cross_point_1, cross_point_2, schedule_1, schedule_2)
