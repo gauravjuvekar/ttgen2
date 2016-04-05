e9cdbbb50743d01b5379439c7ad730078d397d3e
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-04-05 11:23:08 +0530

    Use a proper Gtk.Application

--- src/main.py
+++ src/main.py
@@ -2,7 +2,45 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk, GObject, Gio
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Handlers(object):
+    pass
+
+
+class TTgen2(Gtk.Application):
+    handlers = Handlers()
+
+    def __init__(self, application_id, flags):
+        Gtk.Application.__init__(
+            self,
+            application_id=application_id,
+            flags=flags)
+        self.connect("activate", self.new_window)
+
+    def new_window(self, *args):
+        ApplicationWindow(self, self.handlers)
+
+
+class ApplicationWindow(object):
+    def __init__(self, application, handlers, glade="gui.glade"):
+        self.application = application
+        try:
+            builder = Gtk.Builder.new_from_file(glade)
+            builder.connect_signals(handlers)
+        except GObject.GError:
+            logger.critical("Error reading glade file %s", glade)
+            raise
+        self.main_window = builder.get_object("main_window")
+        self.main_window.set_application(self.application)
+        self.main_window.show()
+
+    def close(self, *args):
+        self.main_window.destroy()
 
 
 class Handlers(object):
@@ -10,14 +48,10 @@ class Handlers(object):
         Gtk.main_quit(*args)
 
 
-def main(builder):
-    window = builder.get_object("main_window")
-    window.show_all()
-    Gtk.main()
+def main():
+    ttgen2 = TTgen2("com.ttgen2.ttgen2", Gio.ApplicationFlags.FLAGS_NONE)
+    ttgen2.run()
 
 
 if __name__ == "__main__":
-    builder = Gtk.Builder()
-    builder.add_from_file("gui.ui")
-    builder.connect_signals(Handlers())
-    main(builder)
+    main()
